8bc70b44cd721ad38d71ebb425d0c22a7df17855
afabiani <alessio.fabiani@geo-solutions.it>
2019-02-04 18:25:36 +0100

    - Gestione Contatti e Aggiornamento Piano

--- strt/serapide_core/api/schema.py
+++ strt/serapide_core/api/schema.py
@@ -30,23 +30,34 @@ from graphene_django.debug import DjangoDebug
 from graphene_django.filter import DjangoFilterConnectionField
 from graphene_file_upload.scalars import Upload
 
-# from graphene_django_extras import DjangoObjectType
-
 from graphql_extensions.exceptions import GraphQLError
 
 from strt_users.models import (
-    AppUser, Organization, OrganizationType
+    AppUser,
+    Organization,
+    OrganizationType,
 )
 
 from serapide_core.helpers import (
-    get_errors, update_create_instance, is_RUP,
+    is_RUP,
+    get_errors,
+    update_create_instance,
 )
 from serapide_core.modello.models import (
-    Piano, Fase, Risorsa, FasePianoStorico, RisorsePiano,
-    ProceduraVAS, RisorseVas,
+    Fase,
+    Piano,
+    Risorsa,
+    Contatto,
+    ProceduraVAS,
+    FasePianoStorico,
+    RisorsePiano, RisorseVas,
+    AutoritaCompetenteVAS, SoggettiSCA,
 )
 from serapide_core.modello.enums import (
-    FASE, TIPOLOGIA_PIANO, TIPOLOGIA_VAS,
+    FASE,
+    TIPOLOGIA_VAS,
+    TIPOLOGIA_PIANO,
+    TIPOLOGIA_CONTATTO,
 )
 
 logger = logging.getLogger(__name__)
@@ -64,12 +75,17 @@ class FasePiano(StrtEnumNode):
     pass
 
 
+class TipologiaVAS(StrtEnumNode):
+    pass
+
+
 class TipologiaPiano(StrtEnumNode):
     pass
 
 
-class TipologiaVAS(StrtEnumNode):
+class TipologiaContatto(StrtEnumNode):
     pass
+
 
 # ############################################################################ #
 # INPUTS                                                                       #
@@ -150,6 +166,7 @@ class AppUserNode(DjangoObjectType):
         exclude_fields = ('password', 'is_staff', 'is_active', 'is_superuser', 'last_login' )
         interfaces = (relay.Node, )
 
+
 class EnteNode(DjangoObjectType):
 
     tipologia_ente = graphene.Field(EnteTipoNode)
@@ -164,6 +181,7 @@ class EnteNode(DjangoObjectType):
                     if _m.organization.code == self.code:
                         roles.append(_m.type.code)
         return roles
+
     class Meta:
         model = Organization
         # Allow for some more advanced filtering here
@@ -185,8 +203,26 @@ class ProceduraVASNode(DjangoObjectType):
         # Allow for some more advanced filtering here
         filter_fields = {
             'ente': ['exact'],
+            'piano__codice': ['exact'],
             'note': ['exact', 'icontains'],
             'tipologia': ['exact', 'icontains'],
+        }
+        interfaces = (relay.Node, )
+
+
+class ContattoNode(DjangoObjectType):
+
+    ente = graphene.Field(EnteNode)
+
+    class Meta:
+        model = Contatto
+        # Allow for some more advanced filtering here
+        filter_fields = {
+            'nome': ['exact', 'icontains'],
+            'email': ['exact'],
+            'tipologia': ['exact', 'icontains'],
+            'ente': ['exact'],
+            'piano__codice': ['exact'],
         }
         interfaces = (relay.Node, )
 
@@ -198,6 +234,7 @@ class PianoNode(DjangoObjectType):
     storico_fasi = graphene.List(FasePianoStoricoType)
     risorsa = DjangoFilterConnectionField(RisorsePianoType)
     procedura_vas = graphene.Field(ProceduraVASNode)
+    soggetto_proponente = graphene.Field(ContattoNode)
 
     def resolve_storico_fasi(self, info, **args):
         # Warning this is not currently paginated
@@ -243,6 +280,17 @@ class EnteCreateInput(InputObjectType):
     code = graphene.String(required=True)
 
 
+class ContattoCreateInput(InputObjectType):
+    """
+    Class created to accept input data
+    from the interactive graphql console.
+    """
+    nome = graphene.String(source='nome', required=True)
+    email = graphene.String(source='email', required=True)
+    tipologia = graphene.String(required=True)
+    ente = graphene.InputField(EnteCreateInput, required=True)
+
+
 class PianoCreateInput(InputObjectType):
     """
     Class created to accept input data
@@ -269,6 +317,9 @@ class PianoUpdateInput(InputObjectType):
     data_avvio = graphene.types.datetime.DateTime(required=False)
     data_approvazione = graphene.types.datetime.DateTime(required=False)
     descrizione = graphene.InputField(graphene.List(graphene.String), required=False)
+    soggetto_proponente_uuid = graphene.String(required=False)
+    autorita_competente_vas = graphene.List(graphene.String, required=False)
+    soggetti_sca = graphene.List(graphene.String, required=False)
 
 
 class ProceduraVASCreateInput(InputObjectType):
@@ -279,6 +330,16 @@ class ProceduraVASCreateInput(InputObjectType):
     tipologia = graphene.String(required=True)
 
     piano = graphene.InputField(PianoUpdateInput, required=False)
+    note = graphene.InputField(graphene.List(graphene.String), required=False)
+    data_creazione = graphene.types.datetime.DateTime(required=False)
+
+
+class ProceduraVASUpdateInput(InputObjectType):
+    """
+    Class created to accept input data
+    from the interactive graphql console.
+    """
+    tipologia = graphene.String(required=False)
     note = graphene.InputField(graphene.List(graphene.String), required=False)
     data_creazione = graphene.types.datetime.DateTime(required=False)
     data_verifica = graphene.types.datetime.DateTime(required=False)
@@ -293,6 +354,7 @@ class ProceduraVASCreateInput(InputObjectType):
 # FILTERS
 # ##############################################################################
 class EnteUserMembershipFilter(django_filters.FilterSet):
+
     # Do case-insensitive lookups on 'name'
     name = django_filters.CharFilter(lookup_expr='iexact')
 
@@ -309,7 +371,26 @@ class EnteUserMembershipFilter(django_filters.FilterSet):
             return super(EnteUserMembershipFilter, self).qs.none()
 
 
+class EnteContattoMembershipFilter(django_filters.FilterSet):
+
+    # Do case-insensitive lookups on 'name'
+    name = django_filters.CharFilter(lookup_expr='iexact')
+
+    class Meta:
+        model = Contatto
+        fields = ['name', 'email', 'ente', ]
+
+    @property
+    def qs(self):
+        # The query context can be found in self.request.
+        if self.request.user and self.request.user.is_authenticated:
+            return super(EnteContattoMembershipFilter, self).qs.filter(ente__usermembership__member=self.request.user)
+        else:
+            return super(EnteContattoMembershipFilter, self).qs.none()
+
+
 class PianoUserMembershipFilter(django_filters.FilterSet):
+
     # Do case-insensitive lookups on 'name'
     codice = django_filters.CharFilter(lookup_expr='iexact')
     fase__codice = django_filters.CharFilter(lookup_expr='iexact')
@@ -343,6 +424,7 @@ class PianoUserMembershipFilter(django_filters.FilterSet):
 # QUERIES
 # ##############################################################################
 class Query(object):
+
     # Models
     enti = DjangoFilterConnectionField(EnteNode,
                                        filterset_class=EnteUserMembershipFilter)
@@ -356,15 +438,25 @@ class Query(object):
 
     procedure_vas = DjangoFilterConnectionField(ProceduraVASNode)
 
+    contatti = DjangoFilterConnectionField(ContattoNode,
+                                           filterset_class=EnteContattoMembershipFilter)
+
     # Enums
     fase_piano = graphene.List(FasePiano)
-    tipologia_piano = graphene.List(TipologiaPiano)
     tipologia_vas = graphene.List(TipologiaVAS)
+    tipologia_piano = graphene.List(TipologiaPiano)
+    tipologia_contatto = graphene.List(TipologiaContatto)
 
     def resolve_fase_piano(self, info):
         l = []
         for f in FASE:
             l.append(FasePiano(f[0], f[1]))
+        return l
+
+    def resolve_tipologia_vas(self, info):
+        l = []
+        for t in TIPOLOGIA_VAS:
+            l.append(TipologiaVAS(t[0], t[1]))
         return l
 
     def resolve_tipologia_piano(self, info):
@@ -373,10 +465,10 @@ class Query(object):
             l.append(TipologiaPiano(t[0], t[1]))
         return l
 
-    def resolve_tipologia_vas(self, info):
+    def resolve_tipologia_contatto(self, info):
         l = []
-        for t in TIPOLOGIA_VAS:
-            l.append(TipologiaVAS(t[0], t[1]))
+        for t in TIPOLOGIA_CONTATTO:
+            l.append(TipologiaContatto(t[0], t[1]))
         return l
 
     # Debug
@@ -436,6 +528,64 @@ class UpdateFase(relay.ClientIDMutation):
             tb = traceback.format_exc()
             logger.error(tb)
             return GraphQLError(e, code=500)
+
+
+class CreateContatto(relay.ClientIDMutation):
+
+    class Input:
+        contatto = graphene.Argument(ContattoCreateInput)
+
+    nuovo_contatto = graphene.Field(ContattoNode)
+
+    @classmethod
+    def mutate_and_get_payload(cls, root, info, **input):
+        try:
+            if is_RUP(info.context.user):
+                _data = input.get('contatto')
+                # Ente (M)
+                if 'ente' in _data:
+                    _ente = _data.pop('ente')
+                    _ente = Organization.objects.get(usermembership__member=info.context.user, code=_ente['code'])
+                    _data['ente'] = _ente
+                # Tipologia (M)
+                if 'tipologia' in _data:
+                    _tipologia = _data.pop('tipologia')
+                    if _tipologia and _tipologia in TIPOLOGIA_CONTATTO:
+                        _data['tipologia'] = _tipologia
+                _contatto = Contatto()
+                nuovo_contatto = update_create_instance(_contatto, _data)
+                return cls(nuovo_contatto=nuovo_contatto)
+            else:
+                return GraphQLError(_("Forbidden"), code=403)
+        except BaseException as e:
+            tb = traceback.format_exc()
+            logger.error(tb)
+            return GraphQLError(e, code=500)
+
+
+class DeleteContatto(graphene.Mutation):
+
+    class Arguments:
+        uuid = graphene.ID(required=True)
+
+    success = graphene.Boolean()
+    uuid =  graphene.ID()
+
+    def mutate(self, info, **input):
+        if is_RUP(info.context.user):
+            # Fetching input arguments
+            _id = input['uuid']
+            try:
+                _contatto = Contatto.objects.get(uuid=_id)
+                _contatto.delete()
+
+                return DeleteContatto(success=True, uuid=_id)
+            except BaseException as e:
+                tb = traceback.format_exc()
+                logger.error(tb)
+                return GraphQLError(e, code=500)
+
+        return DeleteContatto(success=False)
 
 
 class CreatePiano(relay.ClientIDMutation):
@@ -530,6 +680,44 @@ class UpdatePiano(relay.ClientIDMutation):
                     if 'descrizione' in _piano_data:
                         _data = _piano_data.pop('descrizione')
                         _piano.descrizione = _data[0]
+                    # SoggettoProponente (O)
+                    if 'soggetto_proponente_uuid' in _piano_data:
+                        _soggetto_proponente_uuid = _piano_data.pop('soggetto_proponente_uuid')
+                        if len(_soggetto_proponente_uuid) > 0:
+                            _soggetto_proponente = Contatto.objects.get(uuid=_soggetto_proponente_uuid)
+                            _piano.soggetto_proponente = _soggetto_proponente
+                        else:
+                            _piano.soggetto_proponente = None
+                    if 'autorita_competente_vas' in _piano_data:
+                        _autorita_competente_vas = _piano_data.pop('autorita_competente_vas')
+                        if len(_autorita_competente_vas) > 0:
+                            _autorita_competenti = []
+                            for _contatto_uuid in _autorita_competente_vas:
+                                _autorita_competenti.append(AutoritaCompetenteVAS(
+                                    piano=_piano,
+                                    autorita_competente=Contatto.objects.get(uuid=_contatto_uuid)
+                                    )
+                                )
+                            _piano.autorita_competente_vas.clear()
+                            for _ac in _autorita_competenti:
+                                _ac.save()
+                        else:
+                            _piano.autorita_competente_vas.all().delete()
+                    if 'soggetti_sca' in _piano_data:
+                        _soggetti_sca_uuid = _piano_data.pop('soggetti_sca')
+                        if len(_soggetti_sca_uuid) > 0:
+                            _soggetti_sca = []
+                            for _contatto_uuid in _soggetti_sca_uuid:
+                                _soggetti_sca.append(SoggettiSCA(
+                                    piano=_piano,
+                                    soggetto_sca=Contatto.objects.get(uuid=_contatto_uuid)
+                                    )
+                                )
+                            _piano.soggetti_sca.clear()
+                            for _sca in _soggetti_sca:
+                                _sca.save()
+                        else:
+                            _piano.soggetti_sca.all().delete()
                     piano_aggiornato = update_create_instance(_piano, _piano_data)
                     return cls(piano_aggiornato=piano_aggiornato)
             except BaseException as e:
@@ -580,7 +768,7 @@ class CreateProceduraVAS(relay.ClientIDMutation):
 class UpdateProceduraVAS(relay.ClientIDMutation):
 
     class Input:
-        procedura_vas = graphene.Argument(ProceduraVASCreateInput)
+        procedura_vas = graphene.Argument(ProceduraVASUpdateInput)
         uuid = graphene.String(required=True)
 
     errors = graphene.List(graphene.String)
@@ -642,6 +830,7 @@ How to use this mutation
   4. <other POST params if needed>
 """
 class UploadFile(graphene.Mutation):
+
     class Arguments:
         codice_piano = graphene.String(required=True)
         tipo_file = graphene.String(required=True)
@@ -702,6 +891,7 @@ class UploadFile(graphene.Mutation):
 
 
 class DeleteRisorsa(graphene.Mutation):
+
     class Arguments:
         risorsa_id = graphene.ID(required=True)
 
@@ -805,3 +995,5 @@ class Mutation(object):
     create_procedura_vas = CreateProceduraVAS.Field()
     update_procedura_vas = UpdateProceduraVAS.Field()
     upload_risorsa_vas = UploadRisorsaVAS.Field()
+    create_contatto = CreateContatto.Field()
+    delete_contatto = DeleteContatto.Field()
