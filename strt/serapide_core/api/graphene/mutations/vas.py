22248928fd6a9c9acf2ebbc4e2bc2728c150f20f
afabiani <alessio.fabiani@geo-solutions.it>
2019-03-20 15:20:54 +0100

    - Avvio Esame PAreri

--- strt/serapide_core/api/graphene/mutations/vas.py
+++ strt/serapide_core/api/graphene/mutations/vas.py
@@ -461,40 +461,53 @@ class InvioPareriVAS(graphene.Mutation):
         if fase.nome == FASE.anagrafica:
             _pareri_sca = piano.azioni.filter(
                 tipologia=TIPOLOGIA_AZIONE.pareri_sca).first()
-            if _pareri_sca.stato == STATO_AZIONE.attesa:
+            if _pareri_sca.stato != STATO_AZIONE.nessuna:
                 _pareri_sca.stato = STATO_AZIONE.nessuna
                 _pareri_sca.data = datetime.datetime.now(timezone.get_current_timezone())
                 _pareri_sca.save()
 
-                _osservazioni_enti = Azione(
-                    tipologia=TIPOLOGIA_AZIONE.osservazioni_enti,
-                    attore=TIPOLOGIA_ATTORE.enti,
-                    order=_order,
-                    stato=STATO_AZIONE.necessaria
-                )
-                _osservazioni_enti.save()
-                _order += 1
-                AzioniPiano.objects.get_or_create(azione=_osservazioni_enti, piano=piano)
-
-                _osservazioni_regione = Azione(
-                    tipologia=TIPOLOGIA_AZIONE.osservazioni_regione,
-                    attore=TIPOLOGIA_ATTORE.regione,
-                    order=_order,
-                    stato=STATO_AZIONE.attesa
-                )
-                _osservazioni_regione.save()
-                _order += 1
-                AzioniPiano.objects.get_or_create(azione=_osservazioni_regione, piano=piano)
-
-                _upload_osservazioni_privati = Azione(
-                    tipologia=TIPOLOGIA_AZIONE.upload_osservazioni_privati,
+                _avvio_esame_pareri_sca = Azione(
+                    tipologia=TIPOLOGIA_AZIONE.avvio_esame_pareri_sca,
                     attore=TIPOLOGIA_ATTORE.comune,
                     order=_order,
                     stato=STATO_AZIONE.necessaria
                 )
-                _upload_osservazioni_privati.save()
+                _avvio_esame_pareri_sca.save()
                 _order += 1
-                AzioniPiano.objects.get_or_create(azione=_upload_osservazioni_privati, piano=piano)
+                AzioniPiano.objects.get_or_create(azione=_avvio_esame_pareri_sca, piano=piano)
+
+                """
+                    TODO: Queste azioni qui sotto in realtà sono di AVVIO Prodedimento!
+                """
+                # _osservazioni_enti = Azione(
+                #     tipologia=TIPOLOGIA_AZIONE.osservazioni_enti,
+                #     attore=TIPOLOGIA_ATTORE.enti,
+                #     order=_order,
+                #     stato=STATO_AZIONE.necessaria
+                # )
+                # _osservazioni_enti.save()
+                # _order += 1
+                # AzioniPiano.objects.get_or_create(azione=_osservazioni_enti, piano=piano)
+                #
+                # _osservazioni_regione = Azione(
+                #     tipologia=TIPOLOGIA_AZIONE.osservazioni_regione,
+                #     attore=TIPOLOGIA_ATTORE.regione,
+                #     order=_order,
+                #     stato=STATO_AZIONE.attesa
+                # )
+                # _osservazioni_regione.save()
+                # _order += 1
+                # AzioniPiano.objects.get_or_create(azione=_osservazioni_regione, piano=piano)
+                #
+                # _upload_osservazioni_privati = Azione(
+                #     tipologia=TIPOLOGIA_AZIONE.upload_osservazioni_privati,
+                #     attore=TIPOLOGIA_ATTORE.comune,
+                #     order=_order,
+                #     stato=STATO_AZIONE.necessaria
+                # )
+                # _upload_osservazioni_privati.save()
+                # _order += 1
+                # AzioniPiano.objects.get_or_create(azione=_upload_osservazioni_privati, piano=piano)
 
     @classmethod
     def mutate(cls, root, info, **input):
