8525e74785026a9c69e84d0389e278e39da553f0
afabiani <alessio.fabiani@geo-solutions.it>
2019-03-22 11:47:49 +0100

    - Hardening VAS rules

--- strt/serapide_core/api/schema.py
+++ strt/serapide_core/api/schema.py
@@ -138,6 +138,7 @@ class Mutation(object):
 
     create_procedura_avvio = avvio.CreateProceduraAvvio.Field()
     update_procedura_avvio = avvio.UpdateProceduraAvvio.Field()
+    avvia_piano = avvio.AvvioPiano.Field()
 
     upload = uploads.UploadFile.Field()
     delete_risorsa = uploads.DeleteRisorsa.Field()
