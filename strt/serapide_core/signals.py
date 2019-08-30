3c4769f3d905391b42fa0e95b8254e8e88d8adc0
etj <etj@geo-solutions.it>
2019-08-30 11:08:10 +0200

    #282: log email notifications

--- strt/serapide_core/signals.py
+++ strt/serapide_core/signals.py
@@ -9,11 +9,15 @@
 #
 #########################################################################
 
+import logging
+
 from django.dispatch import Signal
 
 from .notifications_helper import send_now_notification
 
 from serapide_core.modello.models import PianoAuthTokens
+
+logger = logging.getLogger(__name__)
 
 # ############################################################################ #
 # Signals
@@ -48,7 +52,17 @@ def message_sent_notification(sender, **kwargs):
                               })
 
 
+def log(piano, notification_type, user_type, user):
+    logger.info("SEND {piano}:{azione} TO {usertype} {user}".format(
+        user=user,
+        usertype=user_type,
+        piano=piano.codice,
+        azione=notification_type))
+
 def piano_phase_changed_notification(sender, **kwargs):
+
+    logger.info("========== piano_phase_changed_notification")
+    #logger.warning(kwargs, stack_info=True)
 
     if 'piano' in kwargs:
         notification_type = kwargs['message_type']
@@ -59,22 +73,27 @@ def piano_phase_changed_notification(sender, **kwargs):
         tokens = []
         if piano.soggetto_proponente:
             users.append(piano.soggetto_proponente.user)
+            log(piano, notification_type, "proponente", piano.soggetto_proponente.user)
 
         if piano.autorita_competente_vas:
             for _c in piano.autorita_competente_vas.all():
                 users.append(_c.user)
+                log(piano, notification_type, "autorita_competente_vas", _c.user)
 
         if piano.autorita_istituzionali:
             for _c in piano.autorita_istituzionali.all():
                 users.append(_c.user)
+                log(piano, notification_type, "autorita_istituzionali", _c.user)
 
         if piano.altri_destinatari:
             for _c in piano.altri_destinatari.all():
                 users.append(_c.user)
+                log(piano, notification_type, "altri_destinatari", _c.user)
 
         if piano.soggetti_sca:
             for _c in piano.soggetti_sca.all():
                 users.append(_c.user)
+                log(piano, notification_type, "soggetti_sca", _c.user)
 
         if PianoAuthTokens.objects.filter(piano=piano).count() > 0:
             tokens = [_p.token for _p in PianoAuthTokens.objects.filter(piano=piano)]
@@ -82,7 +101,7 @@ def piano_phase_changed_notification(sender, **kwargs):
         send_now_notification(users,
                               notification_type,
                               {
-                                "user": from_user,
-                                "piano": piano,
-                                "tokens": tokens
+                                  "user": from_user,
+                                  "piano": piano,
+                                  "tokens": tokens
                               })
