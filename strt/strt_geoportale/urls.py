78016df96b27a9a5a71f2cd865ed15ab0e1f906b
Andrea Cappugi <kappu72@gmail.com>
2020-04-22 16:06:11 +0200

    Aggiunta risorsa di base e configurate le url

--- strt/strt_geoportale/urls.py
+++ strt/strt_geoportale/urls.py
@@ -1,6 +1,12 @@
 from django.urls import path
+from django.conf.urls import include, url
+from django.views.generic import RedirectView
 from .views import geportale_home
 
 urlpatterns = [
     path('', geportale_home, name='geportale_home'),
+    url(r'(static\/extjs\/search\/category\/MAP\/)', RedirectView.as_view(url='/static/MAPS.json', permanent=False)),
+    url(r'(static\/extjs\/search\/category\/CONTEXT\/)', RedirectView.as_view(url='/static/EMPTY_RESOURCES.json', permanent=False)),
+    url(r'(static\/extjs\/search\/category\/GEOSTORY\/)', RedirectView.as_view(url='/static/EMPTY_RESOURCES.json', permanent=False)),
+    url(r'(static\/extjs\/search\/category\/DASHBOARD\/)', RedirectView.as_view(url='/static/EMPTY_RESOURCES.json', permanent=False)),
 ]
