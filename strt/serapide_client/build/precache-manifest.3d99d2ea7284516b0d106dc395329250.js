f9b3cca06a8ddf6fbc99ccac7da9d9ed0de145b3
Andrea Cappugi <kappu72@gmail.com>
2020-05-05 17:51:12 +0200

    Aggiornato il compilato di serapide_client fare collectstatic

---
+++ strt/serapide_client/build/precache-manifest.3d99d2ea7284516b0d106dc395329250.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "3308edc555d6d7d8ff0e",
+    "url": "/static/css/main.767b3ef1.chunk.css"
+  },
+  {
+    "revision": "3308edc555d6d7d8ff0e",
+    "url": "/static/js/main.3308edc5.chunk.js"
+  },
+  {
+    "revision": "d4374ae11790a4cbd071",
+    "url": "/static/css/1.fc7ea443.chunk.css"
+  },
+  {
+    "revision": "d4374ae11790a4cbd071",
+    "url": "/static/js/1.d4374ae1.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "f163e3dc23e7d34eb979ac403d72c2e9",
+    "url": "/index.html"
+  }
+];
