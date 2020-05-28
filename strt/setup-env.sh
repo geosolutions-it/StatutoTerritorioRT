1dbeceebf59baec982e434f541489f5446a70b4b
etj <etj@geo-solutions.it>
2020-05-28 12:02:31 +0200

    Fix for wrong commit

--- strt/setup-env.sh
+++
@@ -1,5 +0,0 @@
-cd /home/geosol/prj/serapide/StatutoTerritorioRT/deployment
-. setenv.sh hepta.env
-cd /home/geosol/prj/serapide/StatutoTerritorioRT/strt
-#DJANGO_DATABASE_URL='sqlite://:memory:' python manage.py test serapide_core.tests.test_etj
-
