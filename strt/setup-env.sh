68300f6071dd63ccf6009a4a741c6e8bb5b66529
etj <etj@geo-solutions.it>
2020-05-28 09:31:47 +0200

    #350 invio mail

---
+++ strt/setup-env.sh
@@ -0,0 +1,5 @@
+cd /home/geosol/prj/serapide/StatutoTerritorioRT/deployment
+. setenv.sh hepta.env
+cd /home/geosol/prj/serapide/StatutoTerritorioRT/strt
+#DJANGO_DATABASE_URL='sqlite://:memory:' python manage.py test serapide_core.tests.test_etj
+
