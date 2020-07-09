cf5f2d0546117f78fea6043953f49369c34c10f7
Andrea Cappugi <kappu72@gmail.com>
2020-07-09 11:47:10 +0200

    Aggiunta autenticazione a geoportale

--- strt/strt_geoportale/urls.py
+++ strt/strt_geoportale/urls.py
@@ -5,8 +5,4 @@ from .views import geportale_home
 
 urlpatterns = [
     path('', geportale_home, name='geportale_home'),
-    url(r'(static\/extjs\/search\/category\/MAP\/)', RedirectView.as_view(url='/static/MAPS.json', permanent=False)),
-    url(r'(static\/extjs\/search\/category\/CONTEXT\/)', RedirectView.as_view(url='/static/EMPTY_RESOURCES.json', permanent=False)),
-    url(r'(static\/extjs\/search\/category\/GEOSTORY\/)', RedirectView.as_view(url='/static/EMPTY_RESOURCES.json', permanent=False)),
-    url(r'(static\/extjs\/search\/category\/DASHBOARD\/)', RedirectView.as_view(url='/static/EMPTY_RESOURCES.json', permanent=False)),
 ]
--- strt/strt_geoportale/views.py
+++ strt/strt_geoportale/views.py
@@ -1,4 +1,19 @@
+# -*- coding: utf-8 -*-
+#########################################################################
+#
+# Copyright 2020, GeoSolutions SAS.
+# All rights reserved.
+#
+# This source code is licensed under the BSD-style license found in the
+# LICENSE file in the root directory of this source tree.
+#
+#########################################################################
 from django.shortcuts import render
+from serapide_core.api.auth.user import (
+    has_profile
+)
+from django.contrib.auth.decorators import user_passes_test
 
+@user_passes_test(has_profile)
 def geportale_home(request):
     return render(request, "geoportale/geoportale.html")
