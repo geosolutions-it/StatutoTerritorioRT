04b445ab4edd4001e883d124fff7e90b60588816
giohappy <giohappy@gmail.com>
2021-02-11 10:41:43 +0100

    fix getUffici PIAN -> OPREG (2)

---
+++ strt/serapide_client/build/precache-manifest.7256511469c3efdec02580ac5dc38a19.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "bbf76f7e4e1794a1209f",
+    "url": "/static/css/main.fcc5b42b.chunk.css"
+  },
+  {
+    "revision": "bbf76f7e4e1794a1209f",
+    "url": "/static/js/main.bbf76f7e.chunk.js"
+  },
+  {
+    "revision": "de48570340f53a3a6733",
+    "url": "/static/css/1.feac13e3.chunk.css"
+  },
+  {
+    "revision": "de48570340f53a3a6733",
+    "url": "/static/js/1.de485703.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "6eb8c4a2dd06ba9701e9f620b4239062",
+    "url": "/index.html"
+  }
+];
